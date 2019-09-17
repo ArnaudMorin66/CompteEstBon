a416ba02258a30b9633644390734988f089593ad
Arnaud Morin <arnaud.morin.66@live.fr>
2019-09-17 12:53:29 +0200

    grille-liste

--- CebBlazor/Startup.cs
+++ CebBlazor/Startup.cs
@@ -2,15 +2,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CebBlazor {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            services.AddRazorPages();
+            services.AddRazorPages(options => {
+                Console.WriteLine(options.RootDirectory);
+                Console.WriteLine(options.Conventions.Count);
+            });
             services.AddScoped<CompteEstBon.CebTirage>();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor(options => {
+                options.DetailedErrors = true;
+                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
